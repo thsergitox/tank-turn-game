bcc4a090727187f49e77e2d7e71e2cfcdc2b092d
Oscar Torres <oscar.torres.m@uni.pe>
2024-10-14 14:23:07 -0500

    add object controller and base object

--- src/app/main.py
+++ src/app/main.py
@@ -1,27 +1,37 @@
 import pygame
+from core import ObjectController
 
 CLOCK = None
 SCREEN_SIZE = (1280, 720)
 SCREEN = None
 RUNNING = True
 
+OBJECT_CONTROLLER = ObjectController()
+
+
 def start():
     global SCREEN, CLOCK
     pygame.init()
     SCREEN = pygame.display.set_mode(SCREEN_SIZE)
     CLOCK = pygame.time.Clock()
+    OBJECT_CONTROLLER.start()
 
 def update():
     global RUNNING, CLOCK, SCREEN
     while RUNNING:
+        SCREEN.fill("skyblue")
+
+        OBJECT_CONTROLLER.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
-        SCREEN.fill("skyblue")
-        pygame.display.flip()
+        
+        pygame.display.update()
         CLOCK.tick(60)
 
 def end():
+    OBJECT_CONTROLLER.end()
     pygame.quit()
 
 if __name__ == "__main__":
