0874da323ff27597c8c9fc1c4e89372f0f309cb3
A-PachecoT <andre.pacheco.t@uni.pe>
2024-10-14 15:22:51 -0500

    feat: health bar

--- src/app/models/tank/BaseTank.py
+++ src/app/models/tank/BaseTank.py
@@ -4,11 +4,12 @@ import math
 
 
 class BaseTank(ABC):
-    def __init__(self, x, y, color, health):
+    def __init__(self, x, y, color, max_health):
         self.x = x
         self.y = y
         self.color = color
-        self.health = health
+        self.max_health = max_health
+        self.health = max_health
         self.angle = 0
         self.size_x, self.size_y = 20, 20
 
@@ -31,11 +32,28 @@ class BaseTank(ABC):
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.size_x, self.size_y))
+        self.draw_health_bar(screen)
 
-    # def draw_health_bar(self, screen):
-    #     health_bar_width = 20
-    #     health_bar_height = 5
-    #     health_bar_x = self.x - health_bar_width / 2
-    #     health_bar_y = self.y - 25
+    def draw_health_bar(self, screen):
+        health_bar_width = 20
+        health_bar_height = 5
 
-    #     pygame.draw.rect(screen, (0, 255, 0), (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
+        # Fixed frame
+        frame_bar_x = self.x + self.size_x / 2 - health_bar_width / 2
+        frame_bar_y = self.y - 2 * health_bar_height
+
+        # Draw frame
+        pygame.draw.rect(
+            screen,
+            (200, 200, 200),
+            (frame_bar_x, frame_bar_y, health_bar_width, health_bar_height),
+        )
+
+        # Health bar
+        health_bar_width = self.health / self.max_health * health_bar_width
+
+        pygame.draw.rect(
+            screen,
+            (0, 255, 0),
+            (frame_bar_x, frame_bar_y, health_bar_width, health_bar_height),
+        )
