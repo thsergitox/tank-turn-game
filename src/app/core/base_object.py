8b6c435116e403fc905d92376eaeef16b86300e8
thsergitox <sergiopezoj@gmail.com>
2024-10-15 17:15:47 -0500

    style: pre-commit

--- src/app/core/base_object.py
+++ src/app/core/base_object.py
@@ -1,10 +1,9 @@
-
 from abc import abstractmethod
 
 import pygame
 
-class BaseObject(pygame.Rect):
 
+class BaseObject(pygame.Rect):
     def __init__(self, object_controller, x, y, width, height):
         super().__init__(x, y, width, height)
         object_controller.list.append(self)
@@ -16,7 +15,6 @@ class BaseObject(pygame.Rect):
     @abstractmethod
     def update(self):
         pass
-        
 
     @abstractmethod
     def end(self):
