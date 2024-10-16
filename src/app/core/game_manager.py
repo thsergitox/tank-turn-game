a52775b88d93758a040549a9e7a7e8542672cda3
Oscar Torres <oscar.torres.m@uni.pe>
2024-10-15 19:41:47 -0500

    game flow working

--- src/app/core/game_manager.py
+++ src/app/core/game_manager.py
@@ -16,18 +16,6 @@ KEY = None
 
 
 class GameManager:
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
     def GetCurrPlayer(self):
         return PLAYERS[TURN]
 
